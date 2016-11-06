545b4ad609f4dd02a530e38af6f20bab7bcfbc27
sgzh <sg@toalaska.net>
2016-11-06 23:37:32 +0800

    增加error()

--- result/result.go
+++ result/result.go
@@ -1,5 +1,7 @@
 package result
 
+import "strconv"
+
 type ReturnResult struct {
 	Code   int `json:"code"`
 	Msg    string `json:"msg"`
@@ -14,4 +16,20 @@ var OK=ReturnResult{
 	Debug:"",
 	Result:nil,
 
+}
+
+func (this *ReturnResult) New(code int,msg string,info string,debug string,result interface{}) ReturnResult {
+	this.Code=code
+	this.Msg=msg
+	this.Info=info
+	this.Debug=debug
+	this.Result=result
+	return this
+}
+//实现了这个方法就可以作为函数执行错误的返回值
+func (this *ReturnResult) Error() string {
+	if this.Info!=""{
+		return this.Info
+	}
+	return this.Msg+"("+strconv.Itoa(this.Code)+")"
 }
