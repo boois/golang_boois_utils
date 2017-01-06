c720ca4e9782e0acb104c003e0f2fcacc664acfe
sgzh <sg@toalaska.net>
2017-01-06 16:44:50 +0800

    增加FATAL级别

--- boois_log/boois_log.go
+++ boois_log/boois_log.go
@@ -7,6 +7,7 @@ import (
 )
 
 const (
+	LV_FATAL = 5
 	LV_ERROR = 4
 	LV_WARN = 3
 	LV_INFO = 2
@@ -14,6 +15,7 @@ const (
 )
 
 var (
+	ins_F *log.Logger
 	ins_E *log.Logger
 	ins_W *log.Logger
 	ins_I *log.Logger
@@ -26,12 +28,18 @@ func SetLv(lv1 int) {
 	lv = lv1
 }
 func init() {
+	ins_F = log.New(os.Stderr, "[FATAL] ", log.LstdFlags | log.Llongfile)
 	ins_E = log.New(os.Stderr, "[ERROR] ", log.LstdFlags | log.Llongfile)
+
 	ins_W = log.New(os.Stdout, "[WARN] ", log.LstdFlags | log.Llongfile)
 	ins_I = log.New(os.Stdout, "[INFO] ", log.LstdFlags | log.Llongfile)
 	ins_D = log.New(os.Stdout, "[DEBUG] ", log.LstdFlags | log.Llongfile)
 }
-
+func F(v ...interface{}) {
+	if LV_FATAL >= lv {
+		ins_F.Output(2, fmt.Sprintln(v...))
+	}
+}
 func E(v ...interface{}) {
 	if LV_ERROR >= lv {
 		ins_E.Output(2, fmt.Sprintln(v...))
